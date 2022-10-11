83ea80329d07c45c949053c683102ba0fadc3496
im2sh <104557685+im2sh@users.noreply.github.com>
2022-10-12 00:20:42 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -5,15 +5,37 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import NotificationList from "./3차시-1/NotificationList";
 import Accommodate from "./3차시-2/Accommodate";
+import ConfirmButton from "./4차시/ConfirmButton";
+import LandingPage from "./4차시/LandingPage";
+import AttendanceBook from "./5차시/AttendanceBook";
+import SignUp from "./5차시/SignUp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <NotificationList />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <NotificationList />
+  // </React.StrictMode>
+
   // <React.StrictMode>
   //   <Accommodate />
   // </React.StrictMode>
+
+  // <React.StrictMode>
+  //   <ConfirmButton />
+  // </React.StrictMode>
+  
+  // <React.StrictMode>
+  //   <LandingPage />
+  // </React.StrictMode>
+
+  // <React.StrictMode>
+  //   <AttendanceBook />
+  // </React.StrictMode>
+
+  <React.StrictMode>
+    <SignUp />
+  </React.StrictMode>
+ 
 );
 
 reportWebVitals();
